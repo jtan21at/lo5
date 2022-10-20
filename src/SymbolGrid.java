c3f5c21e95c10c15b17b700f12533ead35df8d67
tam <jtam6@live.com>
2022-10-20 02:57:39 -0700

    beta

--- src/SymbolGrid.java
+++ src/SymbolGrid.java
@@ -17,41 +17,46 @@ public class SymbolGrid {
         int mark_path=0;
         char temp=0;
 
-        for (int i=0;i< grid.length;i++){
+        for (int i=0;i< grid.length;i++) {
 //            if (index==4) break;
-            for (int j=0;j<grid[i].length;j++){
+            for (int j = 0; j < grid[i].length; j++) {
 
 //
-                if (grid[i][j]==sequence[index]){
-                    index=0;
-                        Cell mycell=new Cell(i,j);
-
-                        mypath.add(mycell);
-
-                        findPathsAt(grid,mycell,mypath,sequence,index+1);}
-
-//                sequence[mark_path]=temp;
-//                for (int k=0;k< sequence.length;k++)
-//                     {if (grid[i][j]==sequence[k]){
-//                         Cell mycell=new Cell(i,j);
-//                         index=0;
+//                if (grid[i][j] == sequence[index]) {
+//                    index = 0;
+//                    Cell mycell = new Cell(i, j);
+//                    temp = sequence[index];
+//                    sequence[index] = 0;
+//                    mypath.add(mycell);
 //
+//                    findPathsAt(grid, mycell, mypath, sequence, index + 1);
+//                    sequence[index] = temp;
+//
+//                }
+
+//                temp = sequence[mark_path];
+                for (int k=0;k< sequence.length;k++)
+                     {if (grid[i][j]==sequence[k]){
+                         Cell mycell=new Cell(i,j);
+                         index=0;
+//                         temp=sequence[k];
 //                         sequence[k]=0;
 //                         mark_path=k;
-//                         temp=sequence[k];
+//
 //                         mypath.add(mycell);
-//
-//                         findPathsAt(grid,mycell,mypath,sequence,index+1);
+
+                         findPathsAt(grid,mycell,mypath,sequence,index);
 //                         sequence[mark_path]=temp;
+
+                     }
 //
-//                     }
-//
-               }
+//               }
 //                if (index>3) break;
 
             }
 //            if (index>3) break;
 //        }
+        }}
         System.out.println("\n--- finished searching");
     }
 
@@ -60,22 +65,43 @@ public class SymbolGrid {
 //
 //     NOTE: You may change the list of parameters here
     private static void findPathsAt(char[][] grid, Cell cell, List<Cell> path, char[] sequence, int index) {
-        if (index > 3) {
-            System.out.println("find one");
-            for (Cell c:path) System.out.print(c);
-            System.out.println();
-            return;
-        }
+
         int mark = 0;
-        char temp = 0;
+        char temp = 0; if(path.contains(cell))return;
         for (int k=0;k< sequence.length;k++) {
+
             if (grid[cell.r][cell.c] == sequence[k]) {
 
                 Cell mycell = new Cell(cell.r, cell.c);
+                if (!path.contains(mycell)) path.add(mycell);
+                else {
+                    return;
+                }
+                if (index > 3) {
+                    System.out.println("find one");
+                    for (Cell c:path) System.out.print(c);
+                    System.out.println();
+                    path.remove(index-1);
+                }
                 temp = sequence[k];
                 mark = k;
                 sequence[k] = 0;
-                path.add(mycell);
+                index++;
+                //temporary remove the element
+//                if(path.contains(mycell))return;
+
+//
+//                if (index > 2) {
+//                    System.out.println("find one");
+//                    for (Cell c : path) System.out.print(c);
+//                    System.out.println();
+//                    path.remove(index);
+//
+//                    sequence[mark] = temp;
+////
+//////            System.exit(index);
+//                    return;
+//                }
 
 
 //        if (grid[cell.r][cell.c] == sequence[index]) {
@@ -108,34 +134,36 @@ public class SymbolGrid {
                 int c = cell.c;
                 int max_c = grid[r].length;
                 int max_r = grid.length;
-                int p_r = path.get(index).r;
-                int p_c = path.get(index).c;
-                if (r - 1 <= grid.length && r - 1 >= 0 && c > 0 && c <= max_c && (r - 1 != p_r || c != p_c))
+                int p_r = path.get(index-1).r;
+                int p_c = path.get(index-1).c;
+                if (r - 1 < grid.length && r - 1 >= 0 && c > 0 && c < max_c && (r - 1 != p_r || c != p_c))
                     findPathsAt(grid, new Cell(cell.r - 1, cell.c), path, sequence, index + 1);//1
-                if (r + 1 <= grid.length && r + 1 >= 0 && c > 0 && c <= max_c && (r + 1 != p_r || c != p_c))
+                if (r + 1 < grid.length && r + 1 >= 0 && c > 0 && c < max_c && (r + 1 != p_r || c != p_c))
                     findPathsAt(grid, new Cell(cell.r + 1, cell.c), path, sequence, index + 1);//2
 
-                if (r >= 0 && r <= max_r && c - 1 >= 0 && c - 1 <= max_c && (r != p_r || c - 1 != p_c))
+                if (r >= 0 && r < max_r && c - 1 >= 0 && c - 1 < max_c && (r != p_r || c - 1 != p_c))
                     findPathsAt(grid, new Cell(cell.r, cell.c - 1), path, sequence, index + 1);//3
-                if (r >= 0 && r <= max_r && c + 1 >= 0 && c + 1 <= max_c && (r != p_r || c + 1 != p_c))
+                if (r >= 0 && r < max_r && c + 1 >= 0 && c + 1 < max_c && (r != p_r || c + 1 != p_c))
                     findPathsAt(grid, new Cell(cell.r, cell.c + 1), path, sequence, index + 1);//4
-                if (r + 1 >= 0 && r + 1 <= max_r && c + 1 >= 0 && c + 1 <= max_c && (r + 1 != p_r || c + 1 != p_c))
+                if (r + 1 >= 0 && r + 1 < max_r && c + 1 >= 0 && c + 1 < max_c && (r + 1 != p_r || c + 1 != p_c))
                     findPathsAt(grid, new Cell(cell.r + 1, cell.c + 1), path, sequence, index + 1);//5
-                if (r - 1 >= 0 && r - 1 <= max_r && c - 1 >= 0 && c - 1 <= max_c && (r - 1 != p_r || c - 1 != p_c))
+                if (r - 1 >= 0 && r - 1 < max_r && c - 1 >= 0 && c - 1 < max_c && (r - 1 != p_r || c - 1 != p_c))
                     findPathsAt(grid, new Cell(cell.r - 1, cell.c - 1), path, sequence, index + 1);//6
-                if (r - 1 >= 0 && r - 1 <= max_r && c + 1 >= 0 && c + 1 <= max_c && (r - 1 != p_r || c + 1 != p_c))
+                if (r - 1 >= 0 && r - 1 < max_r && c + 1 >= 0 && c + 1 < max_c && (r - 1 != p_r || c + 1 != p_c))
                     findPathsAt(grid, new Cell(cell.r - 1, cell.c + 1), path, sequence, index + 1);//7
-                if (r + 1 >= 0 && r + 1 < max_r && c - 1 >= 0 && c - 1 <= max_c && (r + 1 != p_r || c - 1 != p_c))
+                if (r + 1 >= 0 && r + 1 < max_r && c - 1 >= 0 && c - 1 < max_c && (r + 1 != p_r || c - 1 != p_c))
                     findPathsAt(grid, new Cell(cell.r + 1, cell.c - 1), path, sequence, index + 1);//8
-                path.remove(index);
-            }
-            sequence[mark] = temp;// backtrack
+                path.remove(index-1);
+
+
+            sequence[mark] = temp; return;
+            }}// backtrack
         }
 
 
 
 
-    }
+
     public static void main(String[] args) {
         char[][] grid = randomSymbolGrid(7);
         displaySymbolGrid(grid);
